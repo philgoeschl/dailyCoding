41f256cb6024a341dbdd691036cc580c5b5e8298
philgoeschl <contact@philgoeschl.com>
2020-08-24 21:02:45 +0200

    added slider for interval time selection

--- ChristmasLights/index.js
+++ ChristmasLights/index.js
@@ -8,4 +8,8 @@ function StopBlinking() {
     root.style.setProperty('--blinking-state', 'paused');
 }
 
+function updateIntervalTime() {
+    root.style.setProperty('--blinking-interval', document.getElementById('interval-time').value);
+}
+
 
