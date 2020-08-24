01c8d1773dd9e27a478f43d89425edd5f78e0771
philgoeschl <contact@philgoeschl.com>
2020-08-24 20:10:01 +0200

    added start stop functionality and minor styling changes

--- ChristmasLights/index.js
+++ ChristmasLights/index.js
@@ -1,3 +1,11 @@
+let root = document.documentElement;
 
+function StartBlinking() {
+    root.style.setProperty('--blinking-state', 'running');
+}
+
+function StopBlinking() { 
+    root.style.setProperty('--blinking-state', 'paused');
+}
 
 
