960e68f669f2379df5205ccd9c93cee769b84a12
philgoeschl <contact@philgoeschl.com>
2020-10-16 22:18:51 +0200

    init vue-weather-app

---
+++ vue-weather-app/vue-weather-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: '79ab0ca6444d98a9940cae152311b383'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
