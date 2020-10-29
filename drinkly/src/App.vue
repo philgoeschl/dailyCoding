3729241036a19ac64cc32ad3800bcf2739374043
philgoeschl <contact@philgoeschl.com>
2020-10-29 20:29:01 +0100

    search for ingredients and list them on the page

--- drinkly/src/App.vue
+++ drinkly/src/App.vue
@@ -2,20 +2,68 @@
   <div id="app">
     <main>
       <div class="search-box">
-        
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..." 
+          v-model="query" 
+          @keypress="fetchDrinksByIngredient"
+        />
+      </div>
+      <div class="results">
+        <ul id="drink-list">
+          <li v-for="drink in this.drinks.drinks" :key="drink.strDrink">
+            {{drink.strDrink}}
+          </li>
+        </ul>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  name: "app",
+  data() {
+    return {
+      api_key: process.env.VUE_APP_API_KEY,
+      url_base: "https://www.thecocktaildb.com/api/json/v1/",
+      query: "",
+      drinks: {},
+    };
+  },
+  methods: {
+    fetchDrinksByIngredient(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}${this.api_key}/filter.php?i=${this.query}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.drinks = results;
+      console.log(results);
+    }
+  }
 }
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,4 +72,46 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+    background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
 </style>
