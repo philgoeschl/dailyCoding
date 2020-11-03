171a7b5f038127bd050ce32f096b7660657ee470
philgoeschl <contact@philgoeschl.com>
2020-11-03 20:33:44 +0100

    styling changes, added flex

--- drinkly/src/App.vue
+++ drinkly/src/App.vue
@@ -2,20 +2,27 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
-          placeholder="Search..." 
-          v-model="query" 
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
           @keypress="fetchDrinksByIngredient"
         />
       </div>
       <div class="results">
-        <ul id="drink-list">
-          <li v-for="drink in this.drinks.drinks" :key="drink.strDrink">
-            {{drink.strDrink}}
-          </li>
-        </ul>
+        <div
+          class="drink-item"
+          v-for="drink in this.drinks.drinks"
+          :key="drink.idDrink"
+        >
+          <div class="card">
+            <div class="thumbnail">
+              <img :src="drink.strDrinkThumb" />
+            </div>
+            <div class="drink-name">{{ drink.strDrink }}</div>
+          </div>
+        </div>
       </div>
     </main>
   </div>
@@ -35,9 +42,7 @@ export default {
   methods: {
     fetchDrinksByIngredient(e) {
       if (e.key == "Enter") {
-        fetch(
-          `${this.url_base}${this.api_key}/filter.php?i=${this.query}`
-        )
+        fetch(`${this.url_base}${this.api_key}/filter.php?i=${this.query}`)
           .then((res) => {
             return res.json();
           })
@@ -47,13 +52,12 @@ export default {
     setResults(results) {
       this.drinks = results;
       console.log(results);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 * {
   margin: 0;
   padding: 0;
@@ -77,7 +81,7 @@ main {
   min-height: 100vh;
   padding: 25px;
 
-    background-image: linear-gradient(
+  background-image: linear-gradient(
     to bottom,
     rgba(0, 0, 0, 0.25),
     rgba(0, 0, 0, 0.75)
@@ -114,4 +118,43 @@ main {
   border-radius: 16px 0px 16px 0px;
 }
 
+.results {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+
+.drink-item {
+  width: 30vw;
+  margin: 10px;
+}
+
+.card {
+  height: 30vh;
+  overflow: hidden;
+  border-radius: 0px 32px 0px 32px;
+  transition: 0.4s;
+}
+
+.card:hover {
+  border-radius: 32px 0px 32px 0px ;
+}
+
+.thumbnail {
+  
+}
+
+.thumbnail img {
+  object-fit: cover;
+
+}
+
+.thumbnail:hover img {
+  filter: brightness(75%);
+  transition: 0.4s;
+}
+
+.drink-name {
+  z-index: 1;
+}
 </style>
